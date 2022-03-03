aaf391b151a5f1cb0a73d9643e87d0686c7221d8
NodHero <35711894+NodHero@users.noreply.github.com>
2022-03-02 22:05:37 -0500

    Add files via upload

---
+++ Spells/Spells - Book of Lost Spells.js
@@ -0,0 +1,266 @@
+
+
+
+var iFileName = "Book of Lost Spells - SpellsList.js";
+// RequiredSheetVersion(13);
+
+SpellsList["assassin's coin"] = {
+	
+	name : "Assassin’s Coin",
+	source : ["HB", 0],
+	classes : ["warlock", "wizard"],
+	level : 1,
+	school : "Conj",
+	time: "1 a",
+	range: "Touch",
+	components : "V,S,M",
+	compMaterial : "a coin",
+	duration : "Discharge",
+	description : "Returned payment coin returns to caster when holder tosses coin in air and speaks caster's name",
+	descriptionFull :"A coin that you received in payment becomes imbued with a magical charm. Once the spell is cast, you must return the coin to the person or being who gave it to you. As an action, they can throw the coin into the air while speaking your name (or whatever name you gave them). The coin then vanishes and reappears in your pocket, along with a mental image of whoever spoke your name. No message can be transferred along with the coin.",
+	ritual : true,	
+
+
+
+}
+
+SpellsList["air breathing "] = {
+	
+	name : "Air Breathing",
+	source : ["HB", 0],
+	classes : ["druid", "ranger", "sorcerer", "wizards"],
+	level : 3,
+	school : "Trans",
+	time: "1 a",
+	range: "30 ft",
+	components : "V,S,M",
+	compMaterial : "a sliver of copper",
+	duration : "24 h",
+	description : "Up to 10 creatures gain ability to breathe air.",
+	descriptionFull :"You grant up to ten willing creatures you can see the ability to breathe air until the spell ends. These creatures retain the ability to breathe in their normal element.",
+	ritual : true,
+
+
+
+}
+
+
+SpellsList["air bridge"] = {
+
+	name : "Air Bridge",
+	source : ["HB", 0],
+	classes : ["cleric", "druid", "wizard"],
+	level : 3,
+	school : "Trans",
+	time: "1 a",
+	range: "120 ft",
+	components : "V,S,M",
+	compMaterial : "a tiny model bridge",
+	duration : "10 m",
+	description : "Create bridge 60 x 10 ft anchored on solid object.",
+	descriptionFull : "You create an invisible bridge up to 60 feet long and 10 feet wide that connects two points of your choosing. Both ends of the bridge must be within the spell’s range, and both must be solid objects that can support the weight of whatever physical objects cross the bridge. The bridge itself has no weight. Anyone can cross this bridge while it exists, provided they know where it is." + "\n " + "Enhancement: When you cast air bridge using a slot of 5th level or higher, the bridge is substantial only to those you nominate; to all others, it does not exist. This is also true if you cast the spell as a ritual and are capable of casting 5th level spells." + "\n " + "Area of Effect: 1 bridge, 60 ft. long, 10 ft. wide.",
+	ritual : true,	
+
+
+
+}
+
+SpellsList["air forge"] = {
+
+	name : "Air Forge",
+	source : ["HB", 0],
+	classes : ["cleric", "druid", "wizard"],
+	level : 2,
+	school : "Trans",
+	time: "1 a",
+	range: "30 ft",
+	components : "V,S",
+	duration : "8 h",
+	description : "Create a 2 cu ft forge that can melt and soften metals",
+	descriptionFull : "You heat a patch of air to the great temperatures of a forge. You can hold up metal to that area and soften it just as a real forge would and then work the metal appropriately. Only items placed in the heated area by the caster are affected; the heat doesn’t affect anything else, and it can’t be felt by living creatures at all. This spell, along with a portable anvil, greatly lessens the difficulty for a craftsman repairing equipment in the field." +"\n " + "Area of Effect: 2 cubic ft. of air." + "\n " + "Enhancement: If air forge is cast using a spell slot higher than 2nd level, it heats a number of cubic feet of air equal to the spell slot used.",
+	ritual : true,	
+
+
+
+}
+
+SpellsList["anchor"] ={
+
+	name : "Anchor",
+	source : ["HB", 0],
+	classes : ["wizard"],
+	level : 3,
+	school : "Evoc",
+	time: "1 a",
+	range: "Touch",
+	components : "V,S,M",
+	compMaterial : "a small bit of iron and a string",
+	duration : "12 h",
+	description : "Create an anchor & chain made of force to maintain a ship position 1 act to raise or lower anchor",
+	descriptionFull : "You create an anchor and chain of pure force that attach to a ship and maintain its position against current and strong wind. Creatures aboard the ship can raise the anchor by using an action to touch the chain and command it, and they can lower the anchor the same way. The anchor disappears when the spell ends.",
+	ritual : true,	
+
+
+
+}
+
+
+SpellsList["copy"] ={
+
+	name : "Copy",
+	source : ["HB", 0],
+	classes : ["bard", "wizard"],
+	level : 1,
+	school : "Trans",
+	time: "1 a",
+	range: "Touch",
+	components : "V,S,M",
+	compMaterial : "a drop of black ink",
+	duration : "Instantaneous",
+	description : "Create duplicate copy of written document up to 10 pages.",
+	descriptionFull : "This spell creates a perfect duplicate of any written or drawn document that you touch onto blank pages you supply. You can copy up to 10 pages of text with one casting of this spell. Magical writing, including spellbook pages, can’t be copied with this spell.",
+	ritual : true,	
+
+
+
+}
+
+
+
+SpellsList["decrypt"] ={
+
+	name : "Decrypt",
+	source : ["HB", 0],
+	classes : ["bard", "wizard"],
+	level : 1,
+	school : "Trans",
+	time: "1 a",
+	range: "Self",
+	components : "V,S",
+	duration : "Conc, 1 hour",
+	description : "Grants advantage on reading encrypted message.",
+	descriptionFull : "You gain insight into an encrypted message you are holding when you cast this spell. This spell gives you tactical advantage on ability checks you make to decipher the document. It doesn’t grant understanding of languages you don’t know; comprehend languages or similar magic might also be necessary to make the message legible.",
+	ritual : true,	
+
+
+
+}
+
+SpellsList["detect curse"]={
+
+	name : "Detect Curse",
+	source : ["HB", 0],
+	classes : ["cleric", "wizard"],
+	level : 2,
+	school : "Div",
+	time: "1 a",
+	range: "Self",
+	components : "V,S",
+	duration : "Instantaneous",
+	description : "Detects Curses within 30 ft. DC 20 Int or Wis determines exact type and effects",
+	descriptionFull : "You determine whether a creature, object, or area you can see within 30 feet of you is affected by a curse. With a successful DC 20 Intelligence or Wisdom check, you can also determine the exact type of curse and its effects. The spell doesn’t function when used on an artifact." +"\n "+ "Area of Effect: 1 creature, object, or small area within 30 ft. of you",
+	ritual : true,	
+
+
+
+}
+
+
+SpellsList["establish foundation"] ={
+
+	name : "Establish Foundation",
+	source : ["HB", 0],
+	classes : ["druid", "wizard"],
+	level : 7,
+	school : "Trans",
+	time: "1 h",
+	range: "Self",
+	components : "V,S,M",
+	compMaterial : "a granite pebble",
+	duration : "Perm",
+	description : "Raises 60 ft stone circle or square centered on caster 20ft thick",
+	descriptionFull : "You cause stone to rise up from the ground beneath you, in either a circle with a 60-foot diameter or a square with 60-foot sides, centered on you. The stone is solid and stable and perfect for use as a building foundation. The foundation is 20 feet thick, and you can choose to have it flush with the ground or as much as half of it above ground level."+"\n "+"Enhancement: When you cast establish foundation with a 9th-level spell slot, you can raise dirt as well as stone in a 60-foot radius around you; this will effectively fill in marshes or lakes.",
+	ritual : true,	
+
+
+
+}
+
+SpellsList["enhanced oration"] = {
+
+	name : "Enhance Oration",
+	source : ["HB", 0],
+	classes : ["bard", "cleric", "wizard"],
+	level : 1,
+	school : "Illus",
+	time: "1 a",
+	range: "Touch",
+	components : "V,S,M",
+	compMaterial : "a small paper cone",
+	duration : "Conc, 1 min",
+	description : "Grants 1 willing creature to be heard within 300 ft",
+	descriptionFull : "You enhance the voice of one willing creature you touch, allowing that creature to be heard clearly by all creatures that can hear within 300 feet of it, regardless of background noise.",
+	ritual : true,	
+
+
+
+}
+
+SpellsList["gallows tree"] ={
+
+	name : "Gallows Tree",
+	source : ["HB", 0],
+	classes : ["warlock", "wizard"],
+	level : 4,
+	school : "Conj",
+	time: "1 a",
+	range: "Touch",
+	components : "V,S",
+	duration : "1 week",
+	save : "Wis",
+	description : "On failed Wis save creature is linked to structure or tree, on death teleport and bound at object",
+	descriptionFull : "One creature you can see must make a Wisdom saving throw. If it fails, it becomes mystically linked to a structure or tree of your choosing on the same plane. If the creature dies during the duration of the spell, its body is instantly teleported to the structure or tree and bound to it by stout ropes.", 
+	ritual : true,	
+
+
+
+}
+
+SpellsList["ice sled"] ={
+
+	name : "Ice Sled",
+	source : ["HB", 0],
+	classes : ["druid", "wizard"],
+	level : 6,
+	school : "Conj",
+	time: "1 m",
+	range: "50 ft",
+	components : "V,S,M",
+	compMaterial : "a ball of snow",
+	duration : "12 hours",
+	description : "Summon 5 sleds connected by 10ft ice chain, speed: 60 ft",
+	descriptionFull : "You summon up to five sleds that can travel rapidly over snow and ice. Each sled can carry up to 500 lbs. of material, including passengers. The sleds are made of extremely sturdy ice and are connected to each other by 10-foot chains of ice. As an action, you can cause the sleds to travel at a speed of 60 feet in the direction of your choice. The sleds can continue to move as long as one of their runners is on snow or ice. If a sled moves onto an area where there is no snow or ice, or if the spell ends, the sleds shatter and crumble into loose snow." + "\n " + "Area of Effect: 5 sleds", 
+	ritual : true,	
+
+
+
+}
+
+SpellsList["spellcaster's refusal"] = {
+
+	name : "Spellcaster’s Refusal",
+	source : ["HB", 0],
+	classes : ["cleric", "wizard"],
+	level : 5,
+	school : "Abjur",
+	time: "1 a",
+	range: "120 ft",
+	components : "V,S",
+	duration : "24 hours",
+	description : "Create invis barrier max 20 ft wide that no spellcaster can pass",
+	descriptionFull : "You create an invisible barrier that wards one point of entry (a hallway, doorway, window or the like). The entryway can be no more than 20 feet across. You must be able to see the area affected and it must be in range. For the duration of the spell, no creature with spellcasting ability can pass through the portal, but non-spellcasting creatures are not affected. You and up to three creatures you nominate when you cast the spell can pass through the portal as if spellcaster’s refusal was not in effect."+ "\n " + "Area of Effect: 1 portal" + "\n " + "Enhancement: For each spell slot used higher than 5th level, you can create one additional barrier.",
+	ritual : true,	
+
+
+
+}
