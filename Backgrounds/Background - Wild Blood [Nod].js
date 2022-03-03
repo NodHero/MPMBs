209330a70aa555d909e10b20cda0c1e8f7c8826d
NodHero <35711894+NodHero@users.noreply.github.com>
2022-03-03 05:15:13 -0500

    Add files via upload

---
+++ Backgrounds/Background - Wild Blood [Nod].js
@@ -0,0 +1,86 @@
+var iFileName = "Background - Wild Blood [Nod].js"; 
+RequiredSheetVersion(13);
+
+SourceList["NodHB-WB"] = {
+	name : "Scrivner",
+	abbreviation : "NodHB-WB",
+	group : "Nod's Homebrew",
+	date : "2021/12/29"
+};
+
+BackgroundList["wild blood"] = {
+	regExpSearch : /^(?=.*blood)(?=.*wild).*$/i,
+	name : "Wild Blood",
+	source : ["NodHB-WB"],
+	skills : ["Nature", "Survival"],
+	calcChanges : {
+		spellList : [
+			function(spList, spName, spType) {
+				// don't add if this is not a class or a list of spells is already given
+				if (!ClassList[spName] || spList.spells || spList.psionic) return;
+				// if this is an 'extra spell', also test if it uses the class' spell list or not
+				if (spType.indexOf("bonus") !== -1 && (spList.school || !spList["class"] || (spList["class"].indexOf(spName) === -1 && spName !== "fighter"))) return;
+				spList.extraspells = spList.extraspells.concat(["druidcraft", "message", "animal friendship", "speak with animals", "thunderwave", "animal messenger", "beast sense", "shatter", "conjure animals", "sending", "dominate beast", "stoneskin", "awaken"]);
+			},
+			"My background adds extra spells to the spell list(s) of my spellcasting class(es): Druidcraft, Message, Animal Friendship, Speak with Animals, Thunderwave, Animal Messenger, Beast Sense, Shatter, Conjure Animals, Sending, Dominate Beast, Stoneskin, and Awaken."
+		]
+	},
+	gold : 10,
+	equipleft : [
+		["Winter blanket", "", 3],
+		["Herbalism kit", "", 3]
+	],
+	equipright : [
+		["Traveler's clothes", "", 4],
+		["Hunting trap", "", 25],
+		["Beast-hide cloak", "", 1],
+		["Belt pouch (with coins)", "", 1]
+	],
+	languageProfs : [["Elvish, Gnomish, or Sylvan", 1]],
+	toolProfs : ["Herbalism kit"],
+	feature : "Voice in the Wild",
+	trait : [
+		"Unlike people, the beasts of the wild are friends who won't stab me in the back.",
+		"Go ahead and insult me\u2014I dare you.",
+		"I've been isolated for so long that I rarely speak, preferring gestures and the occasional grunt.",
+		"I'm oblivious to etiquette and social expectations.",
+		"I connect everything that happens to me to a grand, cosmic plan.",
+		"I often get lost in my own thoughts and contemplation, becoming oblivious to my surroundings.",
+		"I was, in fact, raised by wild animals.",
+		"HarrRRAAGGHH! [I rarely form a coherent sentence and prefer to express myself by breaking things.]"
+	],
+	ideal : [
+		["Clan", "Clan: My clan is all that really matters. (Any)"],
+		["Anarchy", "Anarchy: No person or law or custom can tell another what to do. (Chaotic)"],
+		["Nature", "Nature: We weren't born tame or domesticated, so we shouldn't have to live that way. (Neutral)"],
+		["Might", "Might: The strongest are meant to dominate the weak. (Evil)"],
+		["Rage", "Rage: AAAAAARRRRggggh! [To live is to feel and express the rage burning in your belly.] (Chaotic)"],
+		["Tradition", "Tradition: The Old Ways must be preserved and upheld. (Any)"]
+	],
+	bond : [
+		"Nothing is more important than the other members of my hermitage, order, or association.",
+		"I entered seclusion to hide from the ones who might still be hunting me. I must someday confront them.",
+		"I'm still seeking the enlightenment I pursued in my seclusion, and it still eludes me.",
+		"I entered seclusion because I loved someone I could not have.",
+		"I am devoted to a sacred site in the midst of the wilderness.",
+		"GrrrRRAAAAGGHH! [I will do anything to prove myself greater than my siblings or ancestors.]"
+	],
+	flaw : [
+		"Now that I've returned to the world, I enjoy its delights a little too much.",
+		"I harbor dark, bloodthirsty thoughts that my isolation and meditation failed to quell.",
+		"I let my need to win arguments overshadow friendships and harmony.",
+		"I like keeping secrets and won't share them with anyone.",	
+		"I'm as stubborn as a boar.",
+		"I'm easily manipulated by people I find attractive.",
+	]
+};
+BackgroundFeatureList["voice in the wild"] = {
+	description: "I can always recall the general layout of natural terrain features around me. I can find a hidden place to rest that is secure enough to conceal me from most natural threats, but not supernatural or magical threats (scrying, mental probing, etc.). Each day I can find food and water for myself and up to five other creatures, provided that the land offers berries, small game, water, and so forth.",
+	source : ["NodHB-WB"]
+};
+/* Wild Blood
+You grew up in the wilds, far from civilization and the comforts of town and technology. Part of a savage society that clings desperately to the Old Ways, the wilds are in your blood—attuned to nature, full of primal rage, and given short shrift by a world consumed with continuing civilization's march of progress. You found quiet, solitude, and perhaps deeper spiritual communion with the wild world. In your time apart from the clamor of society, you've witnessed the migration of herds larger than forests, survived weather more extreme than any city-dweller could comprehend, and enjoyed the solitude of being the only thinking creature for miles in any direction. */
+
+
+
+
