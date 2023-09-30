4fad8de889c6c2909009f356562d99193d3436b4
NodHero <35711894+NodHero@users.noreply.github.com>
2023-09-30 03:43:45 -0400

    Update all_WotC_published-ADDS [13.1.10].js
    add apostrophes, Nod!
    fixed double vision entry in Sword of Zariel

--- all_WotC_published-ADDS [13.1.10].js
+++ all_WotC_published-ADDS [13.1.10].js
@@ -617,17 +617,17 @@ MagicItemsList["stone of golorr"] = { // Waterdeep Dragon Heist
 		name : "Stone of Golorr",
 		note : [
 			"The Stone of Golorr is a glossy, greenish-gray stone that fits in the palm of your hand. The stone is actually an aboleth named Golorr, transformed by magic into an object.",
-			"\nRandom Properties." 
+			"\nRandom Properties.",
 			"   The Stone of Golorr has the following random properties, which you can determine by rolling on the tables in the 'Artifacts' section of the Dungeon Masters Guide:\n\t1 minor beneficial properties\n\t1 minor detrimental property",
-			"\nLegend Lore." 
+			"\nLegend Lore.", 
 			"   The Stone of Golorr has 3 charges and regains 1d3 expended charges daily at dawn. While holding the stone, you can expend 1 of its charges to cast the legend lore spell.\n   By using the stone to cast legend lore, you communicate directly with the aboleth, and it shares its knowledge with you. The aboleth can't lie to you, but the information it provides is often cryptic or vague.\n   The aboleth knows where Lord Neverember's secret vault is located. It also knows that three keys are needed to open the vault and that a gold dragon named Aurinax inhabits the vault and guards its treasures.",
-			"\nFailed Memory." 
+			"\nFailed Memory.", 
 			"   When your attunement to the Stone of Golorr ends, you must make a DC 16 Wisdom saving throw. On a failed save, you lose all memory of the stone being in your possession and all knowledge imparted by it. A remove curse spell cast on you has a 20 percent chance of restoring the lost knowledge and memories, and a greater restoration spell does so automatically.",
-			"\nSentience." 
+			"\nSentience.", 
 			"   The Stone of Golorr is a sentient lawful evil magic item with an Intelligence of 18, a Wisdom of 15, and a Charisma of 18. It has hearing and darkvision out to a range of 120 feet. It can communicate telepathically with the creature that is attuned to it, as long as that creature understands at least one language. In addition, the aboleth learns the greatest desires of any creature that communicates telepathically with the stone.\n   The Stone of Golorr hungers for information and prefers not to remain in the clutches of any creature for too long. Whenever the stone desires a new owner, it demands to be given to another intelligent creature as quickly as possible. If its demands are ignored, it tries to take control of its owner (see 'Sentient Magic Items' in chapter 7 of the Dungeon Master's Guide).",
-			"\nPersonality." 
+			"\nPersonality.", 
 			"   The Stone of Golorr has an alien intellect that is both domineering and hungry for knowledge. It thinks of itself as an ageless and immortal god.",
-			"\nDestroying the Stone." 
+			"\nDestroying the Stone.", 
 			"   While in stone form, the aboleth isn't a creature and isn't subject to effects that target creatures. The Stone of Golorr is immune to all damage. Casting an antipathy/sympathy spell on the stone destroys it if the antipathy effect is selected and the spell is directed to repel aberrations. When the spell is cast in this way, the stone transforms into mucus and is destroyed, and Golorr the aboleth appears in an unoccupied space within 30 feet of the stone's remains. The aboleth is incensed by the stone's destruction, and it attacks all other creatures it can see.",
 		]
 	}],
@@ -651,9 +651,8 @@ MagicItemsList["sword of zariel"] = { // Descent into Avernus
 	dmgres : ["Necrotic", "Radiant"],
 	scoresOverride : [0, 0, 0, 0, 0, 20],
 	speed : { fly : { spd : "fixed90", enc : "fixed80" } },
-	vision : [["Truesight", 60]],
+	vision : [["Truesight", 60], ["Adv. on Insight checks", 0]],
 	advantages : [["Insight", true]],
-	vision : [["Adv. on Insight checks", 0]],
 	action : [["bonus action", " (spread/dim light)"]],
 	description : "The sword sheds bright light in a 5-ft radius and dim light for an additional 5 feet. Fiends have disadvantage on attack rolls made within the bright light, even if they can't see it. You can intensify the sword's light as a bonus action, causing it to shed bright light and dim light each for a 15-ft radius instead. (See Notes)",
 	descriptionFull : "This longsword belonged to the angel Zariel before her fall from grace. Fashioned from celestial steel, it gives off a faint glow and hum. The weapon chooses who can attune to it and who can't. It desires a wielder who embodies bravery and heroism.\n   "+
